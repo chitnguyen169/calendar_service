ce798dab9df81e86b27247af4eb3049acd102305
chitnguyen169 <chi.nguyen.gmb@gmail.com>
2024-12-19 14:17:19 -0500

    Optimise import

--- calendar_service_app/views.py
+++ calendar_service_app/views.py
@@ -4,7 +4,6 @@ import pytz
 from django.conf import settings
 from django.utils import timezone
 from rest_framework import status
-from rest_framework.decorators import action
 from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
